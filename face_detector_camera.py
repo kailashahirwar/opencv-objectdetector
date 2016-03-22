b4bba7694b3a8707fcdc63c251e476f7f225c982
Kailash Ahirwar <ahikailash1@gmail.com>
2016-03-22 16:58:56 +0530

    Face Detector Camera added

---
+++ face_detector_camera.py
@@ -0,0 +1,40 @@
+import cv2
+
+'''
+Face Detector 2: Detects face/faces in frames(webcam)
+classifier: haarcascade_frontalface_default.xml from OpenCV library
+'''
+
+# load classifier
+faceCascade = cv2.CascadeClassifier('classifiers/haarcascade_frontalface_default.xml')
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    # Convert frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.3,
+        minNeighbors=5,
+        minSize=(30, 30)
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
