070edf5e70c2a8dc949f5cb8742fd0320b5b5478
Kailash Ahirwar <ahikailash1@gmail.com>
2016-03-22 14:51:05 +0530

    Face Detector added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Object Detector implemented in python using OpenCV
+
+1. Face Detector which uses haarcascade_frontalface_default.xml classifier from OpenCV library
---
+++ face_detector.py
@@ -0,0 +1,27 @@
+import cv2
+
+'''
+Face Detector
+classifier: haarcascade_frontalface_default.xml from OpenCV library
+'''
+
+# load classifier
+classifier = cv2.CascadeClassifier('classifiers/haarcascade_frontalface_default.xml')
+
+# load image
+img = cv2.imread('test_image1.jpg')
+
+# convert image to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# detect faces
+faces = classifier.detectMultiScale(gray, 1.3, 5)
+
+# draw rectangles on image for every detected face
+for (x,y,w,h) in faces:
+    img = cv2.rectangle(img, (x,y), (x+w,y+h), (255, 0, 0), 2)
+
+# finally show image
+cv2.imshow('img', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
